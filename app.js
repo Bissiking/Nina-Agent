8550d243b3547b5abfe1b4456bb404d08a70418d
Bissiking <34923959+Bissiking@users.noreply.github.com>
2022-05-31 23:01:06 +0200

    A20

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = express();
 // Docker
 var Docker = require('dockerode');
-var docker = new Docker({socketPath: '/var/run/docker.sock'});
+var docker = new Docker({ socketPath: '/var/run/docker.sock' });
 // Variables
 
 
@@ -30,49 +30,49 @@ app.post('/containers', (req, res, next) => {
         filters: {
             name: [containerName]
         },
-       all: true
-       }
-    docker.listContainers(opts, function (err, containers) {
+        all: true
+    }
+    docker.listContainers(opts, function(err, containers) {
         // Conversion de la variable en string
         let ContainerString = containers.toString();
         // Checkup de la variable
-        if(ContainerString == ""){
+        if (ContainerString == "") {
             containers = null;
         }
         // Verification de la variable. Si null erreur
         if (containers !== null) {
-            containers.forEach(function (containerInfo, next) {
+            containers.forEach(function(containerInfo, next) {
                 var container = docker.getContainer(containerInfo.Id);
-               switch (commande) {
-                   case 'start':
-                       if(containerInfo.State === "running"){
-                           return res.status(500).send("Le container est déjà allumé");
-                       }else{
-                           container.start(next);
-                           return res.status(200).send("Démarrage du container");
-                       }                
-                       case 'stop':
-                           if(containerInfo.State === "exited"){
-                               return res.status(500).send("Le container est déjà arrêté");
-                           }else{
-                               container.stop(next);
-                               return res.status(200).send("Arrêt du container - 1 Minute environ");
-                           } 
+                switch (commande) {
+                    case 'start':
+                        if (containerInfo.State === "running") {
+                            return res.status(500).send("Le container est déjà allumé");
+                        } else {
+                            container.start(next);
+                            return res.status(200).send("Démarrage du container");
+                        }
+                    case 'stop':
+                        if (containerInfo.State === "exited") {
+                            return res.status(500).send("Le container est déjà arrêté");
+                        } else {
+                            container.stop(next);
+                            return res.status(200).send("Arrêt du container - 1 Minute environ");
+                        }
 
-                           case 'restart':
-                                if(containerInfo.State === "exited"){
-                                    return res.status(500).send("Le container est déjà arrêté");
-                                }else{
-                                    container.restart(next);
-                                    return res.status(200).send("Redémarrage du container - 1 Minute environ");
-                                }
-       
-                   default:
-                       return res.status(500).send("Commande introuvable");
-               }
+                    case 'restart':
+                        if (containerInfo.State === "exited") {
+                            return res.status(500).send("Le container est déjà arrêté");
+                        } else {
+                            container.restart(next);
+                            return res.status(200).send("Redémarrage du container - 1 Minute environ");
+                        }
+
+
+                    default:
+                        return res.status(500).send("Commande introuvable");
+                }
             });
-        }else{
-            console.log('ERROR 01')
+        } else {
             return res.status(500).send("Le container n'existe pas !")
         }
     });
