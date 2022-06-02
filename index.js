220e7efbaf9dac01ce119a705b36108ceb1694d6
Bissiking <34923959+Bissiking@users.noreply.github.com>
2022-06-02 13:57:40 +0200

    UP

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 const path = './functions/config/config.json';
 const { fork } = require("child_process");
+const fs = require("fs");
 
 if (fs.existsSync(path)) {
-    console.log('ok');
+    // Création de la variable child
     const AgentConfig = require(path);
     const data = AgentConfig.agent;
     // // FORK Module
-    const ForkInit = require('./modules/fork/NEWindex');
+    const ForkInit = require('./functions/config/index');
     ForkInit(data); // HTTP MODULE
+    child.on("close", function(code) {
+        console.log("Alerte - Module arrêté. Redémarrage manuel nécéssaire || Code: " + code);
+    });
 } else {
     console.log('Fichier de config non trouvé');
-    // Création de la variable child
-    if (fs.existsSync(path)) {
-        child.on("close", function(code) {
-            console.log("Alerte - Module arrêté. Redémarrage manuel nécéssaire || Code: " + code);
-        });
-    }
-
-
-
-
-    // // FORK Module
-    // const ForkInit = require('./functions/fork/index');
-
-    // // ------------------------------------------------------ //
-    // // Execute FORK Modules
-
-    // ForkInit('http'); // DISCORD MODULE
+    const ForkInit = require('./functions/config/index');
+    ForkInit(data); // HTTP MODULE
+    child.on("close", function(code) {
+        console.log("Alerte - Module arrêté. Redémarrage manuel nécéssaire || Code: " + code);
+    });
 }
