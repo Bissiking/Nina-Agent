538dee425487ae9986a8b5095f746588503b370e
Bissiking <34923959+Bissiking@users.noreply.github.com>
2022-06-02 19:35:57 +0200

    A10

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const { fork } = require("child_process");
 const fs = require("fs");
 
 if (fs.existsSync(path)) {
-    // Création de la variable child
-    const AgentConfig = require(path);
-    const data = AgentConfig.agent;
-    // // FORK Module
-    const ForkInit = require('./functions/config/index');
-    ForkInit(data); // HTTP MODULE
-    child.on("close", function(code) {
-        console.log("Alerte - Module arrêté. Redémarrage manuel nécéssaire || Code: " + code);
-    });
+    // Vérification de la présence du module fork
+    console.log('Vérification de la présence du module fork');
+    let pathFork = './modules/agent-fork';
+    if (fs.existsSync(pathFork)) {
+        console.log('stepOK');
+    } else {
+        const gitPullOrClone = require('git-pull-or-clone');
+        console.log('Téléchargement et installation du module Fork');
+        gitPullOrClone('https://github.com/BissiGIT/agent-fork.git', './modules', (err) => {
+            console.log(err);
+        });
+    }
 } else {
     console.log('Fichier de config non trouvé');
-    const ForkInit = require('./functions/config/index');
-    ForkInit(data); // HTTP MODULE
+    const child = fork('./functions/config/index'); // HTTP MODULE
     child.on("close", function(code) {
         console.log("Alerte - Module arrêté. Redémarrage manuel nécéssaire || Code: " + code);
     });
